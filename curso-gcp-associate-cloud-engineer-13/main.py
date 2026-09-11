5a5c885c1d833ac135b30ccddcaa872186b35b13
DanielFortea <danielfo95@gmail.com>
2026-09-11 18:07:04 +0200

    cr function

--- curso-gcp-associate-cloud-engineer-13/main.py
+++ curso-gcp-associate-cloud-engineer-13/main.py
@@ -2,4 +2,4 @@ import functions_framework
 
 @functions_framework.http
 def hello_http(request):
-    return '¡Hola desde GCP Cloud Functions!'
+    return '¡Hola desde GCP Cloud Functions, soy Dani!'
